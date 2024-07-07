b10cdb6e27affdadca1f64efe42621a24d472ff0
Dom Christie <christiedom@gmail.com>
2024-07-07 15:02:15 +0100

    Rename app to optimistic ui

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module OptimisticUpdates
+module OptimisticUi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
